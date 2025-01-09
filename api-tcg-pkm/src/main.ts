12f69226f35e0c59e3bb3d504a2df87fd4977161
Ignacio Rojas <irojas.kranio@bicevida.cl>
2025-01-09 13:53:21 -0300

    add .env to gitignore and add swagger

--- api-tcg-pkm/src/main.ts
+++ api-tcg-pkm/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,14 @@ async function bootstrap() {
     origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('TCG Pokemon API')
+    .setDescription('API para obtener información Sets de Pokemon TCG')
+    .setVersion('1.0')
+    .addTag('Pokemon')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
