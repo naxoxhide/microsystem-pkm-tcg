970b3862d13987e7e8696ddd5c2e5ac1bbd8ba40
Ignacio Rojas <irojas.kranio@bicevida.cl>
2025-01-09 13:04:28 -0300

    add .env to nestjs

--- api-tcg-pkm/src/app.module.ts
+++ api-tcg-pkm/src/app.module.ts
@@ -6,9 +6,13 @@ import { Card } from './card/entities/card.entity';
 import { Image } from './card/entities/image.entity';
 import { CardModule } from './card/card.module';
 import { HealthModule } from './health/health.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -20,6 +24,23 @@ import { HealthModule } from './health/health.module';
       synchronize: false,
       logging: true,
     }),
+    // se puede reemplazar por el código de TypeOrmModule.forRoot() para usar el .env
+    // por alguna razón en mi maquina esta tomando valores que no corresponden al .env, por si quieren probar usando el .env
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: (configService: ConfigService) => ({
+    //     type: 'postgres',
+    //     host: configService.get<string>('DATABASE_HOST'),
+    //     port: configService.get<number>('DATABASE_PORT'),
+    //     username: configService.get<string>('DATABASE_USER'),
+    //     password: configService.get<string>('DATABASE_PASSWORD'),
+    //     database: configService.get<string>('DATABASE_NAME'),
+    //     entities: [Set, Card, Image],
+    //     synchronize: false,
+    //     logging: true,
+    //   }),
+    // }),
     SetModule,
     CardModule,
     HealthModule,
