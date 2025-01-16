0793f4d226194e64decd9760a34d3cb20392df2f
Ignacio Rojas <irojas.kranio@bicevida.cl>
2025-01-16 18:34:32 -0300

    add .env to gitignore and add swagger

--- api-tcg-pkm/src/card/card.controller.ts
+++ api-tcg-pkm/src/card/card.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { CardService } from './card.service';
 import { Card } from './entities/card.entity';
+import { ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Card')
 @Controller('card')
 export class CardController {
   constructor(private readonly cardService: CardService) {}
 
   @Get(':setId')
+  @ApiResponse({
+    status: 200,
+    description:
+      'Trae las cartas según el setid agregando query param para filtrar el resultado ',
+    type: Card,
+  })
+  @ApiOperation({
+    summary:
+      'Trae las cartas según el setid agregando query param para filtrar el resultado ',
+  })
   async getCardBySetId(
     @Param('setId') setId: string,
     @Query('rarity') rarity?: string,
